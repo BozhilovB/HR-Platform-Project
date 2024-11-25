665e33ab23c0c1c1a97334e8b5c6572bf2a2a339
BozhilovB <bozhidarbozhilov97@gmail.com>
2024-11-25 15:46:31 +0200

    Initial ASP.NET Core MVC project setup

---
+++ HR-Platform/HR-Platform/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Platform.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
