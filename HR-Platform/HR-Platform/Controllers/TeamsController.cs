2f90ce5d11c1e1aed41bc425e1f4b082640de057
BozhilovB <bozhidarbozhilov97@gmail.com>
2024-11-26 18:41:30 +0200

    Added Create functionality and view for Teams

---
+++ HR-Platform/HR-Platform/Controllers/TeamsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Authorize(Roles = "Admin,Manager,HR,Recruiter")]
+public class TeamsController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public TeamsController(ApplicationDbContext dbContext)
+    {
+        this._context = dbContext;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var teams = await _context.Teams
+            .Include(t => t.Manager)
+            .ToListAsync();
+
+        return View(teams);
+    }
+}
